a89aeb270d6c5e695d2dc71a991aae35bf64bedc
AbdulOlatunde <olatundeabdullah07@gmail.com>
2025-10-18 03:57:45 -0700

    update db.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT) || 5000,
-  mongoUri: process.env.MONGO_URI || "",
+  mongoUri: process.env.MONGO_URI || "mongodb+srv://bellababs16:Olatunde28@cluster0.hosvmg2.mongodb.net/hng-profile-api?retryWrites=true&w=majority",
   email: process.env.USER_EMAIL || "olatundeabdullah07@gmail.com",
   name: process.env.USER_NAME || "Olatunde Abdullah",
   stack: process.env.USER_STACK || "Node.js/Express",
